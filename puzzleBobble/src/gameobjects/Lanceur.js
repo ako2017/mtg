bea4e9dd691f52766d62e7456d5a23c2669da99c
akowal <alexandre.kowal@gmail.com>
2019-06-25 05:35:53 +0200

    maj

--- puzzleBobble/src/gameobjects/Lanceur.js
+++ puzzleBobble/src/gameobjects/Lanceur.js
@@ -7,25 +7,27 @@ class Lanceur extends Phaser.Sprite {
 		super(game,0,0,"lanceur");
 		this.anchor.set(0.5,1);
 		this.fleche = game.make.sprite(0, 0, 'fleche');
-		this.fleche.anchor.set(0.5,1);
+		this.fleche.anchor.set(0,0.5);
 		this.addChild(this.fleche);
 
 	}
 
 	moveLeft() {
-		if(this.fleche.angle < -90) return;
+		if(this.fleche.angle < -180) return;
 		this.fleche.angle--;
 		console.log(this.fleche.angle+'r:'+this.fleche.rotation);
 	}
 
 	moveRight() {
-		if(this.fleche.angle > 90) return;
+		if(this.fleche.angle > 0) return;
 		this.fleche.angle++;
 		console.log(this.fleche.angle+'r:'+this.fleche.rotation);
 	}
 
 	fire() {
-		return this.game.add.sprite(234, 32, 'bille');
+		let x = this.x + (Math.cos(this.fleche.rotation)*32);
+		let y = this.y + (Math.sin(this.fleche.rotation)*32);
+		return this.game.add.sprite(x, y, 'bille');
 	}
 
 } 
