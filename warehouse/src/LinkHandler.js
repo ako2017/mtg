a9d826006192134ca5ce6a5ecd3c9fd1bb11b0cc
akowal <alexandre.kowal@swisslife.fr>
2019-07-08 18:20:58 +0200

    maj

--- warehouse/src/LinkHandler.js
+++ warehouse/src/LinkHandler.js
@@ -7,23 +7,51 @@ class LinkHandler {
         this.game = game;
         this.sourceNode = null;
         this.destNode = null;
+        this.poolInput = this.game.add.group();
+        this.poolInput.createMultiple(4,'input');
+        this.poolOutput = this.game.add.group();
+        this.poolOutput.createMultiple(4,'output');
 	}
 
     onClick(node) {
-        console.log(node.x);
-        //this.game.add.sprite(node.x-node.width/2-10, node.y, 'input');
-       // this.game.add.sprite(node.x+node.width/2, node.y, 'output');
-
+        this.showIO(node);
         if(this.sourceNode != null) {
             var dist = Phaser.Math.distance(this.sourceNode.x, this.sourceNode.y,node.x, node.y);
             var deg = Phaser.Math.radToDeg(Phaser.Math.angleBetween(this.sourceNode.x, this.sourceNode.y,node.x, node.y));
             var line = this.game.add.tileSprite(this.sourceNode.x, this.sourceNode.y, dist, 5, 'line');
             line.angle = deg;
+            this.sourceNode = null;
+            this.poolInput.forEachAlive(this.destroy,this);
+            this.poolOutput.forEachAlive(this.destroy,this);
         }
-        this.sourceNode = node;
+        else {
+            this.sourceNode = node;
+        }
     }
 
     onOver(node) {
+        if(this.sourceNode != null) {
+            this.showIO(node);
+        }
+    }
+
+    destroy(node) {
+        node.kill();
+    }
+
+    showIO(node) {
+        var input = this.poolInput.getFirstDead();
+        if(input != null) {
+            input.reset();
+            input.x = node.x-node.width/2-10;
+            input.y = node.y;
+        }
+        var output = this.poolOutput.getFirstDead();
+        if(output != null) {
+            output.reset();
+            output.x = node.x+node.width/2;
+            output.y = node.y;
+        }
     }
 
 } 
