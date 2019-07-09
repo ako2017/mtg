c2784e1025db15f9c5703d76a98d90aa537df653
akowal <alexandre.kowal@swisslife.fr>
2019-07-09 18:16:10 +0200

    maj

--- warehouse/src/LinkHandler.js
+++ warehouse/src/LinkHandler.js
@@ -11,6 +11,7 @@ class LinkHandler {
         this.poolInput.createMultiple(4,'input');
         this.poolOutput = this.game.add.group();
         this.poolOutput.createMultiple(4,'output');
+        this.line = null;
 	}
 
     onClick(node) {
@@ -47,24 +48,55 @@ class LinkHandler {
         arrow.scale.set(1);
     }
 
+
+/*
+this.background.input.startDrag = function(pointer) {
+    pointer.playerStart = new Phaser.Point(self.player.x, self.player.y);
+    Phaser.InputHandler.prototype.startDrag.call(this, pointer);
+};
+this.background.input.updateDrag = function(pointer) {
+    self.player.x = pointer.playerStart.x - pointer.positionDown.x + pointer.x;
+    self.player.y = pointer.playerStart.y - pointer.positionDown.y + pointer.y;
+};
+*/
+
+
     showIO(node) {
         var input = this.poolInput.getFirstDead();
         if(input != null) {
             input.reset();
             input.inputEnabled  = true;
             input.input.enableDrag(true);
-            input.events.onInputOver.add(this.onOverArrow, this);
-            input.events.onInputOut.add(this.onOutArrow, this);
+            //input.events.onInputOver.add(this.onOverArrow, this);
+           // input.events.onInputOut.add(this.onOutArrow, this);
 
             input.x = node.x-node.width/2-10;
             input.y = node.y;
+
+            input.events.onDragStart.add(function(sprite, pointer) {
+                var dist = Phaser.Math.distance(input.x, input.y,input.x+50, input.y);
+                var deg = Phaser.Math.radToDeg(Phaser.Math.angleBetween(input.x, input.y,input.x+10, input.y));
+                this.line = this.game.add.tileSprite(input.x, input.y, dist, 5, 'line');
+                this.line.angle = deg;
+            },this);
+ 
+
+            input.events.onDragUpdate.add(function(sprite, pointer) {
+                this.line.width = pointer.x;
+            },this);
+
+            input.events.onDragStop.add(function(sprite, pointer) {
+                this.line.width=200;
+            },this);
+
+            
         }
         var output = this.poolOutput.getFirstDead();
         if(output != null) {
             output.reset();
             output.inputEnabled  = true;
-            output.events.onInputOver.add(this.onOverArrow, this);
-            output.events.onInputOut.add(this.onOutArrow, this);
+           // output.events.onInputOver.add(this.onOverArrow, this);
+           // output.events.onInputOut.add(this.onOutArrow, this);
             output.x = node.x+node.width/2;
             output.y = node.y;
         }
